3e7016ee0633535fd1d81978d10db7fa501fda26
d-smith <doug.smith.mail@gmail.com>
2018-01-16 13:25:23 -0800

    Added basic email send

--- streamhook.py
+++ streamhook.py
@@ -1,5 +1,28 @@
+import boto3
 
+# Note: sender and recipient email addresses must be verified in the 
+# SES sandbox
+ses_client = boto3.client('ses')
 
 def lambda_handler(event, context):
-    print event
-    print context
+    ses_client.send_email(
+        Destination={
+            'ToAddresses': [
+                'doug.smith@fmr.com',
+            ],
+        },
+        Message={
+            'Body': {
+                'Text': {
+                    'Charset': 'UTF-8',
+                    'Data': "Hey there",
+                },
+            },
+            'Subject': {
+                'Charset': 'UTF-8',
+                'Data': 'Status Update',
+            },
+        },
+        Source='a045103@fmr.com'
+
+    )    
